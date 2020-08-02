e19dfb4e4663d40ec4c8ebec4861967eb395d327
Theodore Moreland <theodoremoreland@yahoo.com>
2020-08-02 01:14:32 -0500

    Finished Class based MVP

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Button from './Button.js'
+import './App.css';
+
+const buttonTextItems = ["Bears, beets, Battlestar Galactica"
+  ,"What's Forrest Gump's password? 1Forrest1"
+  , "Where do programmers like to hangout? The Foo Bar"
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userText: "",
+    };
+    this.input = React.createRef();
+  }
+
+  updateUserText = (event) => {
+    this.setState( {userText : event.target.value } );
+
+    if (event.target.value === this.state.snippet) { 
+      this.setState({victory: true, endTime: new Date().getTime() - this.state.startTime});
+    }
+    else {
+      this.setState({victory: false, endTime: null});
+    }
+  };
+
+  chooseSnippet = (index, event) => {
+    this.setState({snippet : buttonTextItems[index]
+      , startTime: new Date().getTime()
+      , userText: ""
+      , victory: false});
+    this.input.current.focus();
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Type Race</h2>
+        {this.state.snippet}
+        <h4>{this.state.victory ? `Jam Jamboree! Time: ${this.state.endTime}ms` : null}</h4>
+        <hr/>
+        <input value={this.state.userText} onChange={this.updateUserText} ref={this.input}/>
+        <hr/>
+        { buttonTextItems.map((textItem, index) => <Button onClick={(e) => this.chooseSnippet(index, e)} buttonText={textItem} />) }
+      </div>
+    );
+  }
+
+}
+
+export default App;
---
+++ src/Button.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Button = (props) => {
+    return <button onClick={props.onClick}>{props.buttonText}</button>
+}
+
+export default Button;
