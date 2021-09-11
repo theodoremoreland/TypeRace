913d879fcb396d3fde5328a437b94b86f1f66d9b
Theodore Moreland <theodoremoreland@yahoo.com>
2021-09-10 20:05:29 -0500

    removed semantic ui button and card components in favor of custom ones

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,21 +1,18 @@
 // React
 import React from 'react';
 
-// Semantic UI
-import { Button } from 'semantic-ui-react';
-
 // Custom styles
 import './Button.css';
 
 export default function ButtonWrapper({text, callback}) {
 
     return (
-        <Button
+        <button
             key={text}
             className="button"
             onClick={() => callback(text)}
         >
             {text}
-        </Button>
+        </button>
     );
 };
