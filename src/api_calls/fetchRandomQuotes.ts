6f225c8d685de25a3ffd0d1633a1e728c0501cf2
theodoremoreland <theodoremoreland@yahoo.com>
2023-10-03 19:08:15 -0500

    implemented zod

--- src/api_calls/fetchRandomQuotes.ts
+++ src/api_calls/fetchRandomQuotes.ts
@@ -1,10 +1,26 @@
+import { z } from 'zod';
+
+const quoteableSchema = z.object({
+    _id: z.string().optional(),
+    content: z.string().min(6),
+    author: z.string().optional(),
+    tags: z.string().array().optional(),
+    authorSlug: z.string().optional(),
+    length: z.number().optional(),
+    dateAdded: z.coerce.date().optional(),
+    dateModified: z.coerce.date().optional(),
+});
+
+type Quoteable = z.infer<typeof quoteableSchema>;
+
 export const fetchRandomQuotes = async (): Promise<string[]> => {
     const randomizedQuotes: string[] = [];
 
     do {
         const response: Response = await fetch('https://api.quotable.io/random');
-        const json = (await response.json()) as { content: string };
-        const quote: string = json.content;
+        const json: unknown = await response.json();
+        const parsedJson: Quoteable = quoteableSchema.parse(json);
+        const quote: string = parsedJson.content;
 
         if (!randomizedQuotes.includes(quote)) {
             randomizedQuotes.push(quote);
